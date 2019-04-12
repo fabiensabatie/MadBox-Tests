d1a4d4110590b39db9cef11b450b97cef7e2e334
Fabien Sabatié <fabiensabatie@Fabiens-MacBook-Air.local>
2019-04-12 19:59:57 +0200

    Done

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const App					= Express();
 const BodyParser			= require('body-parser');
 const Server				= require('http').createServer(App);
 const Rfr					= require('rfr');
-const SocketIO				= require('socket.io')(Server);
 const Globals				= Rfr('app_commons/globals_cm');
 
 App.use('/', Express.static(__dirname + '/public'));
 App.set('view engine', 'pug');
 
-global.__SOCKETIO = SocketIO;
 exports.App		= App;
 exports.Server	= Server;
