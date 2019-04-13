2c1827aff65bf12f8fcf8f0a9650d42ffb0dddb6
Fabien Sabatié <fabiensabatie@Fabiens-MacBook-Air.local>
2019-04-13 17:24:27 +0200

    Done

--- app_commons/globals_cm.js
+++ app_commons/globals_cm.js
@@ -24,7 +24,7 @@ global.__MONGO_ACTIVE_DBS			= {};
 global.__MONGO_WORDS_COLLECTION		= 'words';
 
 // Game management environment
-global.__WORDLIST_PATH  = ('/Users/fabiensabatie/Projects/MadBox/verbe.txt');
+global.__WORDLIST_PATH  = (__dirname + '/verbe.txt');
 global.__WL = [];
 global.__API_KEY = "trnsl.1.1.20190412T003823Z.f986ceeaa9058f8c.3c5a105dc5941daa55c85c7613ce8fa5535449bd";
 global.__TRANSLATION_URL = `https://translate.yandex.net/api/v1.5/tr.json/translate?&lang=fr-en&key=${__API_KEY}&text=`;
